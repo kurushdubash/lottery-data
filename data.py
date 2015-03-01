5ecb6300a0bb3f7bd75321a3178be4694a00e22f
William Cheung <william0cheung@gmail.com>
2015-03-01 14:03:31 -0800

    added the ny five and mega data

--- data.py
+++ data.py
@@ -1,6 +1,20 @@
+import requests
+
+# api ask a server for data and it will give you that data.
+# api's are usually URLs. 
+
+#URL : https://data.ny.gov/resource/d6yy-54nr.json
+
+nysixnum = "https://data.ny.gov/resource/d6yy-54nr.json"
+nyfivemeg = "https://data.ny.gov/resource/5xaw-6ayf.json"
+
+
+
 def get_lottery_data(URL):
-	"""Returns data for Lottery"""
-	return 0
+	"""Returns data (which is list of dictionaries) for Lottery"""
+	data = requests.get(URL) #one bigass list where each item is a dictionary. 
+	data = data.json() #from object to dictionary
+	return data
 
 def get_lottery_numbers():
 	"""Returns an array of the frequency of each number in the lottery"""
