cf27ddb9d225f3e96d04413baea73f2de349c373
Kurush Dubash <dubash.kurush@gmail.com>
2015-03-01 11:41:03 -0800

    Added data.py, made skeleton code

---
+++ data.py
@@ -0,0 +1,27 @@
+def get_lottery_data(URL):
+	"""Returns data for Lottery"""
+	return 0
+
+def get_lottery_numbers():
+	"""Returns an array of the frequency of each number in the lottery"""
+	return 0
+
+def get_mega_numbers():
+	"""Returns an array of mega numbers"""
+	return 0
+
+def parse_data(data):
+	return 0
+
+def probability(lottery_array):
+	"""Takes in a tuple where lottery_array[0] is an array of 5 get_array_of_numbers
+		and lottery_array[1] is the mega number"""
+	return 0
+
+def random_lottery():
+	"""Retursn a tuple of which the first element is an array of 5 random,
+		non duplicate numbers, and the second element is a mega number"""
+	return 0
+
+
+
