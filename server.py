1ec302072320fbb54250a46cff4b47f7584380fe
Kurush Dubash <dubash.kurush@gmail.com>
2015-03-01 20:22:52 -0800

    Fixed UI, alignment is centered

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return app.send_static_file('index2.html')
+    return app.send_static_file('index.html')
 
 
 @app.route("/api/lottery", methods=["POST"])
